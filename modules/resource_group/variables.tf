9af0d812d230a5266f648597706a171843b30b58
Ayyappan.Selvam <ayyappan.selvam@valgenesis.com>
2025-11-15 11:41:56 +0530

    Initial commit

---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type        = string
+  description = "Name of the Resource Group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
